cb3eed15ac97a39d7ce1ed6d09522a9384d71826
ljor <leah.m.jorgensen@gmail.com>
2021-08-04 22:27:42 -0400

    Added timers

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log("Good to go!")
+let petName = ""
 class Pet {
     constructor(name){
       this.name = name
@@ -6,44 +6,79 @@ class Pet {
       this.hunger = 0
       this.sleepiness = 0
       this.boredom = 0
-    }
-    ageIncrement() {
-        let ageInterval = setInterval(()=>{
-            if (this.age < 35) {
-            this.age++
-            document.querySelector('#age').innerText = `${this.age}`
-            }
-            else if (this.age >= 35){
-                clearInterval(ageInterval)
-                this.deathCheck()
-            }
-        }, 3000)
-    }
-    hungerIncrement() {
-        let hungerInterval = setInterval(()=> {
-            if (this.hunger < 10) {
-                this.hunger++
-                document.querySelector('#hunger').innerText = `${this.hunger}`
-                }
-                else if (this.hunger >= 10){
-                    clearInterval(hungerInterval)
-                    this.deathCheck()
-                }
-        }, 1000)
-        
+      this.counter = 0
     }
 
-    deathCheck() {
-      if (this.age === 35 || this.hunger === 10 || this.sleepiness === 10 || this.boredom === 10) {
+    lifeCycle() {
+        let timeInterval = setInterval(()=>{
+          this.deathCheck(timeInterval)
+          this.counter++
+          if(this.counter % 2 === 0) {
+            this.hunger++
+            document.querySelector('#hunger').innerText = `${this.hunger}`
+          }
+          if(this.counter % 3 === 0) {
+            this.boredom++
+            document.querySelector('#boredom').innerText = `${this.boredom}`
+          }
+          if(this.counter % 4 === 0){
+            this.sleepiness++
+            document.querySelector('#tiredness').innerText = `${this.sleepiness}`
+          }
+          if(this.counter % 6 === 0) {
+          this.age++
+          document.querySelector('#age').innerText = `${this.age}`
+          }
+        }, 500)
+    }
+
+    feed() {
+      if (this.hunger > 0) {
+        this.hunger--
+        document.querySelector('#hunger').innerText = `${this.hunger}`
+        document.querySelector('#message > p').innerText = '*nom nom nom*'
+      }
+    }
+
+    play() {
+      if (this.boredom > 0) {
+        this.boredom--
+        document.querySelector('#boredom').innerText = `${this.boredom}`
+        document.querySelector('#message > p').innerText = '*happy noises*'
+      }
+    }
+
+    lights() {
+      if (this.sleepiness > 0) {
+        this.sleepiness++
+        document.querySelector('#tiredness').innerText = `${this.sleepiness}`
+        document.querySelector('#message > p').innerText = '*zzz*'
+      }
+    }
+
+    deathCheck(timer) {
+      if (this.hunger === 10 || this.sleepiness === 10 || this.boredom === 10 || this.age === 35) {
         document.querySelector('.pet').style.animation = 'none'
         document.querySelector('#message > p').innerText = '*is dead*'
+        document.querySelector('#feed').disabled = true
+        document.querySelector('#lights').disabled = true
+        document.querySelector('#play').disabled = true
         console.log(`${this.name} has died`)
+        clearInterval(timer)
       }
     }
   }
   
+  const newPet = () => {
+    petName = document.getElementById('pet-name').value
+    const myPet = new Pet(petName)
+  }
+
   const testPet = new Pet("Momo")
   console.log(testPet)
-  testPet.deathCheck()
-  testPet.ageIncrement()
-  testPet.hungerIncrement()
+  testPet.lifeCycle()
+
+  document.querySelector('#feed').addEventListener('click', ()=>{testPet.feed()})
+  document.querySelector('#play').addEventListener('click', ()=>{testPet.play()})
+  document.querySelector('#lights').addEventListener('click', ()=>{testPet.lights()})
+  document.querySelector('#new-pet').addEventListener('click', ()=>{newPet()})
